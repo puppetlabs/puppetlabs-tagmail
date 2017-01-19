5d874559b28c4eea07a49b736c0cb4ee00569b76
Wilson McCoubrey <wilson@puppet.com>
2017-01-19 16:51:12 +0000

    Implement beaker-module_install_helper

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,22 +1,22 @@
 require 'beaker-rspec'
 require 'beaker/puppet_install_helper'
+require 'beaker/module_install_helper'
 
 run_puppet_install_helper
+install_ca_certs unless ENV['PUPPET_INSTALL_TYPE'] =~ /pe/i
+install_module_on(hosts)
+install_module_dependencies_on(hosts)
 
 UNSUPPORTED_PLATFORMS = ['windows','Solaris','Darwin']
 
 RSpec.configure do |c|
-  # Project root
-  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-
   # Readable test descriptions
   c.formatter = :documentation
 
   # Configure all nodes in nodeset
   c.before :suite do
-    # Install module and dependencies
+    # Install test dependencies
     hosts.each do |host|
-      copy_module_to(host, :source => proj_root, :module_name => 'tagmail')
       on host, puppet('module install puppetlabs-inifile'), { :acceptable_exit_codes => [0,1] }
     end
   end
