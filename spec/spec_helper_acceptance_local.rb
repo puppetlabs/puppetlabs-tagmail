38ab58530f567eb8f8e3be5c8bd7e8b7054b2615
Daniel Carabas <daniel.carabas@puppet.com>
2020-05-18 15:34:49 +0300

    Update spec_helper_acceptance_local.rb

--- spec/spec_helper_acceptance_local.rb
+++ spec/spec_helper_acceptance_local.rb
@@ -1,6 +1,13 @@
+require 'singleton'
+
+class LitmusHelper
+  include Singleton
+  include PuppetLitmus
+end
+
 UNSUPPORTED_PLATFORMS = ['windows', 'Solaris', 'Darwin'].freeze
 RSpec.configure do |c|
   c.before :suite do
-    run_shell('puppet module install puppetlabs-inifile')
+    LitmusHelper.instance.run_shell('puppet module install puppetlabs-inifile')
   end
 end
